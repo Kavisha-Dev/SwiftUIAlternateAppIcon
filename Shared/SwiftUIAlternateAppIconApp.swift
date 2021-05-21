c0d1e264f5cf67b642a2ce75714d19fbf4311444
kavisha <kavisha_jg@yahoo.com>
2021-05-21 14:47:05 +0530

    Initial Commit

---
+++ Shared/SwiftUIAlternateAppIconApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAlternateAppIconApp.swift
+//  Shared
+//
+//  Created by Kavisha Sonaal on 21/05/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAlternateAppIconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
